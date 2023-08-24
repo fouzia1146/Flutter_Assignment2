a19f2900c52db7e3dd13933e92d7f771ef8ac7b9
fouzia1146 <76620272+fouzia1146@users.noreply.github.com>
2023-08-24 20:16:03 +0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,223 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(CalculatorApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class CalculatorApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculator',
-      home: Calculator(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class Calculator extends StatefulWidget {
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
   @override
-  _CalculatorState createState() => _CalculatorState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _CalculatorState extends State<Calculator> {
-  TextEditingController _controller = TextEditingController();
-
-  void _onOperatorClicked(String operator) {
-    _controller.text += operator;
-  }
-
-  void _onClearClicked() {
-    _controller.clear();
-  }
-
+class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Calculator')),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Expanded(
-            child: Container(
-              alignment: Alignment.bottomRight,
-              padding: EdgeInsets.all(16),
-              child: TextField(
-                controller: _controller,
-                textAlign: TextAlign.right,
-                style: TextStyle(fontSize: 48, fontWeight: FontWeight.bold),
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                ),
-              ),
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          const TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Enter',
             ),
           ),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              _buildOperatorButton('+'),
-              _buildOperatorButton('-'),
-              _buildOperatorButton('*'),
-              _buildOperatorButton('/'),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                   primary: Colors.white,
+                   backgroundColor: Colors.black,
+                ),
+                child:
+                  Text('1', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('2', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('3', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.orange,
+                ),
+                child:
+                Text('+', style: TextStyle(fontSize: 40)),
+              ),
             ],
           ),
+
           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              _buildClearButton(),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('4', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('5', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('6', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.orange,
+                ),
+                child:
+                Text('-', style: TextStyle(fontSize: 40)),
+              ),
+            ],
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('7', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('8', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('9', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.orange,
+                ),
+                child:
+                Text('*', style: TextStyle(fontSize: 40)),
+              ),
+            ],
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('0', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.black,
+                ),
+                child:
+                Text('.', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.orange,
+                ),
+                child:
+                Text('=', style: TextStyle(fontSize: 40)),
+              ),
+              TextButton(
+                onPressed: () {},
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: Colors.orange,
+                ),
+                child:
+                Text('/', style: TextStyle(fontSize: 40)),
+              ),
             ],
           ),
         ],
+      )
       ),
-    );
-  }
-
-  Widget _buildOperatorButton(String operator) {
-    return ElevatedButton(
-      onPressed: () => _onOperatorClicked(operator),
-      child: Text(operator, style: TextStyle(fontSize: 24)),
-    );
-  }
-
-  Widget _buildClearButton() {
-    return ElevatedButton(
-      onPressed: _onClearClicked,
-      child: Text('Clear', style: TextStyle(fontSize: 24)),
     );
   }
 }
